4bd238b31368d1fa805544efbb6e3e19a52101d0
Christos Balaktsis <balaktsis@csd.auth.gr>
2025-03-17 10:12:47 +0200

    Update +page.svelte

--- demo/src/routes/+page.svelte
+++ demo/src/routes/+page.svelte
@@ -24,35 +24,6 @@
 </script>
 
 
-<div class="page-container">
-	<ion-card class="card-container">
-		<div class="header">
-			<ion-icon icon={listCircle}></ion-icon>
-			<h1>My Task List</h1>
-		</div>
-
-		<div class="search-container">
-			<ion-searchbar
-				placeholder="Search tasks"
-				value={searchQuery}
-				on:ionInput={(e) => searchQuery = e.detail.value}>
-			</ion-searchbar>
-		</div>
-
-		{#if $tasks.length > 0}
-			{#each $tasks.filter(task => task.title.toLowerCase().includes(searchQuery.toLowerCase())) as task, i}
-				<TaskCard {task} onRemove={() => removeTask(i)} />
-			{/each}
-		{:else}
-			<p style="text-align: center; color: #666; padding: 1rem;">No tasks found.</p>
-		{/if}
-
-		<ion-button expand="block" color="primary" on:click={() => goto('/create')}>
-			<ion-icon slot="start" icon={add}></ion-icon>
-			Create Task
-		</ion-button>
-	</ion-card>
-</div>
 
 
 <style>
